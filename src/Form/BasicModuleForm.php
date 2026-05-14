187b8711a52f192b3eefd54c4bc1a5b123105d13
Rachana-paudel11 <rachanapaudel2061@gmail.com>
2026-05-14 20:47:43 +0530

    added image field widget, and sent data to database

--- src/Form/BasicModuleForm.php
+++ src/Form/BasicModuleForm.php
@@ -5,6 +5,7 @@ namespace Drupal\basic_module\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Database\Database;
+use Drupal\file\Entity\File;
 
 class BasicModuleForm extends FormBase {
 
@@ -21,6 +22,19 @@ class BasicModuleForm extends FormBase {
       '#required' => TRUE,
     ];
 
+    //Image Field
+    $form['image'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Add an image'),
+      '#required' => TRUE,
+      '#description' => $this->t('Please upload a JPG, PNG, or GIF image. Maximum size: 2 MB.'),
+      '#upload_location' => "public://documents",
+      '#upload_validators' => [
+        'FileExtension' => ['extensions' => 'png jpg jpeg gif'],
+        'FileSizeLimit' => ['fileLimit' => 2 * 1024 * 1024],
+      ],
+
+    ];
     // Description field
     $form['description'] = [
       '#type' => 'textarea',
@@ -46,23 +60,35 @@ class BasicModuleForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Get uploaded file ID
+    $image_id = $form_state->getValue('image')[0];
+    if($image_id){
+      //Load file entity
+      $file = File::load($image_id);
 
-    // Get database connection
-    $connection = Database::getConnection();
+      //Make file Permanent
+      $file->setPermanent();
+      $file->save();
+      // Get database connection
+      $connection = Database::getConnection();
 
-    // Insert data into database
-    $connection->insert('basic_module_data')
-      ->fields([
-        'title' => $form_state->getValue('title'),
-        'description' => $form_state->getValue('description'),
-      ])
-      ->execute();
+      // Insert data into database
+      $connection->insert('basic_module_data')
+          ->fields([
+            'title' => $form_state->getValue('title'),
+            'description' => $form_state->getValue('description'),
+            'image' => $image_id
+          ])
+          ->execute();
 
-    // Success message
-    $this->messenger()->addMessage(
-      $this->t('Data saved successfully for: @title', [
-        '@title' => $form_state->getValue('title')
-      ])
-    );
-  }
+        // Success message
+        $this->messenger()->addMessage(
+          $this->t('Data saved successfully for: @title', [
+            '@title' => $form_state->getValue('title')
+          ])
+        );
+      }
+
+    }
+    
 }
